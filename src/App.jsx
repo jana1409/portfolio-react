14d362f030fcbd142310eace4bd3ed084afa5589
Janani Shri <jananishri2005@gmail.com>
2024-09-29 15:15:25 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-//import {ThemeProvider} from '@material-ui/styles';
-import MainRouter from '../MainRouter'
-//import Contact from './contact'
-//import theme from '../theme';
-//import {hot} from 'react-hot-loader'
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainRouter from '../MainRouter';
 import Navbar from '../components/NavBar/navbar';
 import Intro from '../components/Intro/intro';
 import Skills from '../components/Skills/skills';
-import Contact from "../components/Contact/contact";
+import Contact from '../components/Contact/contact';
 import Works from '../components/Works/works';
 import Footer from '../components/Footer/footer';
 
-
 const App = () => {
-  return(
+  return (
     <Router>
-      <MainRouter />
-      <Navbar />  
-      <Intro/>
-      <Skills />
-      <Contact/>
-      <Works />
-      <Footer />
-      
+      <div>
+        <Navbar />
+        {/* MainRouter is usually responsible for defining route paths */}
+        <MainRouter />
+        {/* Common components like Intro, Skills, etc., can be rendered within specific routes or globally */}
+        <Intro />
+        <Skills />
+        <Works />
+        <Contact />
+        <Footer />
+      </div>
     </Router>
   );
 }
