851344333dac00fba5506036fef0cbe472d74127
Janani Shri <jananishri2005@gmail.com>
2024-10-03 10:25:05 -0400

    Initial commit

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -6,6 +6,7 @@ import Contact from './src/contact'
 import Education from './src/education'
 import Project from './src/project'
 import Layout from './components/Layout'
+import App from './src/App'
 
 
 const MainRouter = () => {
@@ -17,8 +18,9 @@ const MainRouter = () => {
             <Route exact path="/education" element={<Education />} />
             <Route exact path="/project" element={<Project />} />
             <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/App" element={<App />} />
         </Routes>
     </div>
-    )
+    );
 }
-export default MainRouter
+export default MainRouter;
